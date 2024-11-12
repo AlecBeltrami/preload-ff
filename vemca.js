a28c07e81df3570766eaaa648cc970258ccb3567
Alec Beltrami <alecbeltrami@gmail.com>
2024-11-12 10:08:10 -0300

    Update vemca.js

--- vemca.js
+++ vemca.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
+svgImage.src = 'https://alecbeltrami.github.io/preload-ff/vemca.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
